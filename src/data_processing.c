fe798ad5751c540cc6fa23d7e88502d426223bae
Otto White <ow20@ic.ac.uk>
2021-05-29 23:57:26 +0100

    Otto: implement operand2 immediate processing

--- src/data_processing.c
+++ src/data_processing.c
@@ -5,9 +5,9 @@
 #include "definitions.h"
 #include "instructions.h"
 
-void print_binary(uint16_t number) {
-	printf("%d =\t\t", number);
-	for (uint16_t i = 0, mask = 1 << 15; i < 16; i++, number <<= 1) {
+void print_binary(uint32_t number) {
+	printf("%u =\t\t", number);
+	for (uint32_t i = 0, mask = 1 << 31; i < 32; i++, number <<= 1) {
 		printf("%i", ((number & mask) == 0) ? 0 : 1);
 	}
 	printf("\n");
@@ -33,6 +33,7 @@ void apply_operation(enum Opcode opcode,
 		uint32_t operand2, 
 		uint32_t *result, 
 		bool *write_result) {
+	*write_result = true;
 	switch (opcode) {
 		// THINK ABOUT OVERFLOWS
 		case AND: 
@@ -71,6 +72,50 @@ void apply_operation(enum Opcode opcode,
 	}
 }
 
+void process_operand2_immediate_value(uint32_t *operand2_processed_ptr, uint32_t operand2) {
+		// bits 0-7 are an unsigned 8 bit number bits 8-11 are a shift to Imm
+		uint32_t immediate		  = operand2 & 0xff; // with implicit zero extension
+		uint32_t right_rotation_amount	  = 2 * ((operand2 & 0xf00) >> 8);
+		uint32_t arithmetic_shifted_right = immediate >> right_rotation_amount;
+		uint32_t overflow_bits		  = immediate << 32 - right_rotation_amount;
+		*operand2_processed_ptr		  = arithmetic_shifted_right | overflow_bits;
+}
+
+void process_operand2_shifted_register(struct State *state_ptr, 
+		uint32_t *operand2_processed_ptr, 
+		uint32_t operand2) {
+
+		printf("Operand 2:\t");
+		print_binary(operand2);
+
+		uint32_t rm_index = operand2 & 0xf;
+		printf("Rm index:\t");
+		print_binary(rm_index);
+
+		uint32_t rm = state_ptr->registers.array_access[rm_index];
+		printf("Rm:\t\t");
+		print_binary(rm);
+
+		uint32_t shift = (operand2 & 0xff0) >> 4;
+		printf("Shift:\t\t");
+		print_binary(shift);
+
+		// do shift specified by constant amount, maybe do specified by register later
+}
+
+void process_operand2(struct State *state_ptr,
+		 uint32_t *operand2_processed_ptr, 
+		 uint32_t operand2, 
+		 bool immediate_operand) {
+	if (immediate_operand) {
+		process_operand2_immediate_value(operand2_processed_ptr, operand2);
+	} else {
+		process_operand2_shifted_register(state_ptr, operand2_processed_ptr, operand2);
+	}
+}
+
+
+
 void data_processing(struct State *state_ptr,
 		enum Opcode opcode,
 		bool immediate_operand,
@@ -78,26 +123,14 @@ void data_processing(struct State *state_ptr,
 		uint8_t rn,
 		uint8_t rd,
 		uint32_t operand2) {
-
-	// retrieve operand1 from the register file
+	// consider overflows, flag setting
+	bool write_result;
+	uint32_t operand2_processed;
 	uint32_t operand1 = state_ptr->registers.array_access[rn];
+	uint32_t result   = 0;
 	// assert only the bottome 12 bits of operand2 used?
 
-	/*
-	// calculate operand2 (temp: as unshifted immediate value)
-	if (immediate_operand) {
-		// process operand2 as immediate value
-		
-	} else {
-		// process operand2 as shifted register
-
-	}
-	*/
-
-
-	// Split into another function, operand2 processing, consider overflows, flag setting
-	uint32_t result = 0;
-	bool write_result = true;
+	process_operand2(state_ptr, &operand2_processed, operand2, immediate_operand);
 	apply_operation(opcode, operand1, operand2, &result, &write_result);
 
 	// calculate result by applying a function corresponding to the opcode,
@@ -108,7 +141,6 @@ void data_processing(struct State *state_ptr,
 	}
 
 	if (write_result) state_ptr->registers.array_access[rd] = result;
-	return;
 }
 
 int main(void) {
@@ -116,18 +148,21 @@ int main(void) {
 	memset(&state, 0, sizeof(struct State));
 
 	struct Instruction instruction;
-	instruction.immediate_operand = true; 
+	instruction.immediate_operand = false; 
 	instruction.set_condition_codes = true;
 	instruction.rn = 1; // number from 0-12 for register containing first operand
 	instruction.rd = 3; // number from 0-12 for destination register
-	state.registers.array_access[instruction.rn] = 1234;
+
+	state.registers.array_access[3] = 8754;
+
+	state.registers.array_access[instruction.rn] = 8754;
 	instruction.opcode			     = AND;
-	instruction.operand2			     = 10232; 
+	instruction.operand2			     = 3267; 
 
 
-	print_binary(state.registers.array_access[instruction.rn]);
-	print_binary(instruction.operand2);
-	printf("------------------------------------------\n");
+	// print_binary(state.registers.array_access[instruction.rn]);
+	// print_binary(instruction.operand2);
+	// printf("------------------------------------------\n");
 
 	data_processing(&state,
 			instruction.opcode,
@@ -137,7 +172,7 @@ int main(void) {
 			instruction.rd,
 			instruction.operand2);
 
-	print_binary(state.registers.array_access[instruction.rd]);
+	// print_binary(state.registers.array_access[instruction.rd]);
 
 	return 0;
 }
