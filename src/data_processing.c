6dd46f9f191f5a704121d805cb95fa5c5ebb512b
Otto White <ow20@ic.ac.uk>
2021-05-29 17:42:57 +0100

    Otto: create setup for writing data processing instruction

--- src/data_processing.c
+++ src/data_processing.c
@@ -1,7 +1,9 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 #include "definitions.h"
-#include "instructions.h"
+// #include "instructions.h"
 
 void data_processing(struct State *state_ptr,
 		enum Opcode opcode,
@@ -12,3 +14,29 @@ void data_processing(struct State *state_ptr,
 		uint32_t operand2) {
 	return;
 }
+
+int main(void) {
+	printf("Hello world\n");
+	struct State state;
+	struct Instruction instruction;
+	memset(&state, 0, sizeof(struct State));
+
+	instruction.opcode = AND;
+	instruction.immediate_operand = true; 
+	instruction.set_condition_codes = true;
+	instruction.rn = 1; // number from 0-12 for register containing first operand
+	instruction.rd = 3; // number from 0-12 for destination register
+	instruction.operand2 = 15; 
+
+
+	data_processing(&state,
+			instruction.opcode,
+			instruction.immediate_operand,
+			instruction.set_condition_codes,
+			instruction.rn,
+			instruction.rd,
+			instruction.operand2);
+
+	return 0;
+}
+
