2f92024baeb0c44adff480894d7ab7b3347c391f
Otto White <ow20@ic.ac.uk>
2021-05-30 15:46:52 +0100

    Otto: implement operand2 register processing, most shifts

--- src/data_processing.c
+++ src/data_processing.c
@@ -5,120 +5,65 @@
 #include "definitions.h"
 #include "instructions.h"
 
-void print_binary(uint32_t number) {
-	printf("%u =\t\t", number);
-	for (uint32_t i = 0, mask = 1 << 31; i < 32; i++, number <<= 1) {
-		printf("%i", ((number & mask) == 0) ? 0 : 1);
-	}
-	printf("\n");
-}
-void binary_to_denary(char str[]) {
-	int count = 0;
-	for (char *str_ptr = str; *str_ptr != '\0'; str_ptr += 1) {
-		count += 1;
-	}
-
-	int total = 0;
-	int counter = 1;
-	for (int i = count - 1; i >= 0; i--) {
-		char current = *(str + i);
-		if (current == '1') { total += counter; }
-		counter *= 2;
-	}
-	printf("%s = %d\n", str, total);
-}
-
-void apply_operation(enum Opcode opcode, 
-		uint32_t operand1, 
-		uint32_t operand2, 
-		uint32_t *result, 
-		bool *write_result) {
-	*write_result = true;
-	switch (opcode) {
-		// THINK ABOUT OVERFLOWS
-		case AND: 
-			*result = operand1 & operand2;
-			break;
-		case EXCLUSIVE_OR: 
-			*result = operand1 ^ operand2;
-			break;
-		case SUBTRACT: 
-			*result = operand1 - operand2;
-			break;
-		case REVERSE_SUBTRACT: 
-			*result = operand2 - operand1;
-			break;
-		case ADD: 
-			*result = operand1 + operand2; 
-			break;
-		case TEST_BITS: 
-			*result = operand1 & operand2;
-			*write_result = false;
-			break;
-		case TEST_EQUALS: 
-			*result = operand1 ^ operand2;
-			*write_result = false;
-			break;
-		case COMPARE: 
-			*result = operand1 - operand2;
-			*write_result = false;
-			break;
-		case OR: 
-			*result = operand1 | operand2;
-			break;
-		case MOVE: 
-			*result = operand2;
-			break;
-	}
-}
-
-void process_operand2_immediate_value(uint32_t *operand2_processed_ptr, uint32_t operand2) {
-		// bits 0-7 are an unsigned 8 bit number bits 8-11 are a shift to Imm
-		uint32_t immediate		  = operand2 & 0xff; // with implicit zero extension
-		uint32_t right_rotation_amount	  = 2 * ((operand2 & 0xf00) >> 8);
-		uint32_t arithmetic_shifted_right = immediate >> right_rotation_amount;
-		uint32_t overflow_bits		  = immediate << 32 - right_rotation_amount;
-		*operand2_processed_ptr		  = arithmetic_shifted_right | overflow_bits;
-}
-
-void process_operand2_shifted_register(struct State *state_ptr, 
-		uint32_t *operand2_processed_ptr, 
-		uint32_t operand2) {
-
-		printf("Operand 2:\t");
-		print_binary(operand2);
-
-		uint32_t rm_index = operand2 & 0xf;
-		printf("Rm index:\t");
-		print_binary(rm_index);
-
-		uint32_t rm = state_ptr->registers.array_access[rm_index];
-		printf("Rm:\t\t");
-		print_binary(rm);
-
-		uint32_t shift = (operand2 & 0xff0) >> 4;
-		printf("Shift:\t\t");
-		print_binary(shift);
-
-		// do shift specified by constant amount, maybe do specified by register later
-}
-
-void process_operand2(struct State *state_ptr,
-		 uint32_t *operand2_processed_ptr, 
-		 uint32_t operand2, 
-		 bool immediate_operand) {
-	if (immediate_operand) {
-		process_operand2_immediate_value(operand2_processed_ptr, operand2);
-	} else {
-		process_operand2_shifted_register(state_ptr, operand2_processed_ptr, operand2);
-	}
-}
-
-
-
 void data_processing(struct State *state_ptr,
 		enum Opcode opcode,
 		bool immediate_operand,
+		bool set_condition_codes,
+		uint8_t rn,
+		uint8_t rd,
+		uint32_t operand2);
+void apply_operation(enum Opcode opcode, 
+		uint32_t operand1, 
+		uint32_t operand2, 
+		uint32_t *result_ptr, 
+		bool *write_result_ptr); 
+uint32_t process_operand2(struct State *state_ptr, uint32_t operand2, bool immediate_operand);
+uint32_t process_operand2_immediate_value(uint32_t operand2); 
+uint32_t process_operand2_shifted_register(struct State *state_ptr, uint32_t operand2);
+enum Shift_Type get_shift_type(uint32_t operand2);
+uint32_t get_shift_amount(struct State *state_ptr, uint32_t operand2);
+uint32_t shift(uint32_t rm, uint32_t shift_code, uint32_t shift_amount);
+uint32_t logical_shift_left(uint32_t n, uint32_t spaces); // PLACEHOLDER
+uint32_t logical_shift_right(uint32_t n, uint32_t spaces); // PLACEHOLDER
+uint32_t arithmetic_shift_right(uint32_t n, uint32_t spaces); // PLACEHOLDER  
+uint32_t rotate_right(uint32_t n, uint32_t spaces); 
+
+void print_binary(uint32_t number);
+
+int main(void) {
+	struct State state;
+	memset(&state, 0, sizeof(struct State));
+
+	struct Instruction instruction;
+	instruction.immediate_operand = false; 
+	instruction.set_condition_codes = true;
+	instruction.rn = 1; // number from 0-12 for register containing first operand
+	instruction.rd = 10; // number from 0-12 for destination register
+
+	//			            109876543210
+	state.registers.array_access[0] = 0b111111111111;
+	state.registers.array_access[1] = 0b000000000000;
+	state.registers.array_access[2] = 0b000000000000;
+	state.registers.array_access[3] = 0b000000000000;
+
+	//						 109876543210
+	state.registers.array_access[instruction.rn] = 0b000011011011;
+	instruction.opcode			     = AND;
+	//						 109876543210
+	instruction.operand2			     = 0b000001110011; 
+
+	data_processing(&state,
+			instruction.opcode,
+			instruction.immediate_operand,
+			instruction.set_condition_codes,
+			instruction.rn,
+			instruction.rd,
+			instruction.operand2);
+	return 0;
+}
+
+void data_processing(struct State *state_ptr,
+		enum Opcode opcode, bool immediate_operand,
 		bool set_condition_codes,
 		uint8_t rn,
 		uint8_t rd,
@@ -129,8 +74,7 @@ void data_processing(struct State *state_ptr,
 	uint32_t operand1 = state_ptr->registers.array_access[rn];
 	uint32_t result   = 0;
 	// assert only the bottome 12 bits of operand2 used?
-
-	process_operand2(state_ptr, &operand2_processed, operand2, immediate_operand);
+	operand2_processed = process_operand2(state_ptr, operand2, immediate_operand);
 	apply_operation(opcode, operand1, operand2, &result, &write_result);
 
 	// calculate result by applying a function corresponding to the opcode,
@@ -143,37 +87,120 @@ void data_processing(struct State *state_ptr,
 	if (write_result) state_ptr->registers.array_access[rd] = result;
 }
 
-int main(void) {
-	struct State state;
-	memset(&state, 0, sizeof(struct State));
-
-	struct Instruction instruction;
-	instruction.immediate_operand = false; 
-	instruction.set_condition_codes = true;
-	instruction.rn = 1; // number from 0-12 for register containing first operand
-	instruction.rd = 3; // number from 0-12 for destination register
-
-	state.registers.array_access[3] = 8754;
-
-	state.registers.array_access[instruction.rn] = 8754;
-	instruction.opcode			     = AND;
-	instruction.operand2			     = 3267; 
+uint32_t process_operand2(struct State *state_ptr,
+		 uint32_t operand2, 
+		 bool immediate_operand) {
+	if (immediate_operand) {
+		return process_operand2_immediate_value(operand2);
+	} else {
+		return process_operand2_shifted_register(state_ptr, operand2);
+	}
+}
 
 
-	// print_binary(state.registers.array_access[instruction.rn]);
-	// print_binary(instruction.operand2);
-	// printf("------------------------------------------\n");
+void apply_operation(enum Opcode opcode, 
+		uint32_t operand1, 
+		uint32_t operand2, 
+		uint32_t *result_ptr, 
+		bool *write_result_ptr) {
+	*write_result_ptr = true;
+	switch (opcode) {
+		// THINK ABOUT OVERFLOWS
+		case AND: 
+			*result_ptr = operand1 & operand2;
+			break;
+		case EXCLUSIVE_OR: 
+			*result_ptr = operand1 ^ operand2;
+			break;
+		case SUBTRACT: 
+			*result_ptr = operand1 - operand2;
+			break;
+		case REVERSE_SUBTRACT: 
+			*result_ptr = operand2 - operand1;
+			break;
+		case ADD: 
+			*result_ptr = operand1 + operand2; 
+			break;
+		case TEST_BITS: 
+			*result_ptr = operand1 & operand2;
+			*write_result_ptr = false;
+			break;
+		case TEST_EQUALS: 
+			*result_ptr = operand1 ^ operand2;
+			*write_result_ptr = false;
+			break;
+		case COMPARE: 
+			*result_ptr = operand1 - operand2;
+			*write_result_ptr = false;
+			break;
+		case OR: 
+			*result_ptr = operand1 | operand2;
+			break;
+		case MOVE: 
+			*result_ptr = operand2;
+			break;
+	}
+}
 
-	data_processing(&state,
-			instruction.opcode,
-			instruction.immediate_operand,
-			instruction.set_condition_codes,
-			instruction.rn,
-			instruction.rd,
-			instruction.operand2);
+uint32_t process_operand2_immediate_value(uint32_t operand2) {
+	// bits 0-7 are an unsigned 8 bit number bits 8-11 are a shift to Imm
+	uint32_t immediate		  = operand2 & 0xff; // with implicit zero extension
+	uint32_t right_rotation_amount	  = 2 * ((operand2 & 0xf00) >> 8);
+	return rotate_right(immediate, right_rotation_amount);
+}
 
-	// print_binary(state.registers.array_access[instruction.rd]);
+uint32_t process_operand2_shifted_register(struct State *state_ptr, uint32_t operand2) {
+	uint32_t rm_index	   = operand2 & 0xf;
+	uint32_t rm		   = state_ptr->registers.array_access[rm_index];
+	uint32_t shift_code = (operand2 & 0b1100000) >> 5;
+	uint32_t shift_amount = get_shift_amount(state_ptr, operand2);
+	return shift(rm, shift_code, shift_amount);
+}
 
+uint32_t get_shift_amount(struct State *state_ptr, uint32_t operand2) {
+	bool bit_4 = (operand2 & 0b10000) != 0;
+	if (bit_4) {
+		// Register case, bits 8-11 are the register, we use bottom byte of it
+		uint8_t shift_register_index  = (operand2 & 0xf00) >> 8;
+		uint32_t shift_register_value = state_ptr->registers.array_access[shift_register_index];
+		return				(shift_register_value & 0xff);
+	} else {
+		// Integer case, bits 7-11 are the shift
+		return ((operand2 & 0b111110000000) >> 7);
+	}
+}
+
+uint32_t shift(uint32_t rm, uint32_t shift_code, uint32_t shift_amount) {
+	switch (shift_code) {
+		case 0: return logical_shift_left(rm, shift_amount); break;
+		case 1: return logical_shift_right(rm, shift_amount); break;
+		case 2: return arithmetic_shift_right(rm, shift_amount); break;
+		case 3: return rotate_right(rm, shift_amount); break;
+	}
+}
+
+uint32_t logical_shift_left(uint32_t n, uint32_t spaces) {
+	return n << spaces;	
+}
+
+uint32_t logical_shift_right(uint32_t n, uint32_t spaces) {
+	return n >> spaces;
+}
+
+uint32_t arithmetic_shift_right(uint32_t n, uint32_t spaces) {
 	return 0;
 }
 
+uint32_t rotate_right(uint32_t n, uint32_t spaces) {
+	uint32_t arithmetic_shifted_right = n >> spaces;
+	uint32_t overflow_bits		  = n << (32 - spaces);
+	return (arithmetic_shifted_right | overflow_bits);
+}
+
+void print_binary(uint32_t number) {
+	printf("%u =\t\t", number);
+	for (uint32_t i = 0, mask = 1 << 31; i < 32; i++, number <<= 1) {
+		printf("%i", ((number & mask) == 0) ? 0 : 1);
+	}
+	printf("\n");
+}
