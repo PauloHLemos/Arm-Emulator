e556b2d7f640f617c57e37e50d49dde1894b1f69
wz1620 <wz1620@imperial.ac.uk>
2021-05-30 14:36:38 +0100

    Weizhong: make loader byte addressable and fetch return ordered instruction

--- src/fetch.c
+++ src/fetch.c
@@ -2,6 +2,9 @@
  #include "definitions.h"
 
 uint32_t fetch(State *state) {
-		return state.memory[(state.registers.PC/4)];
+		uint32_t pc = state.registers.state_access.PC/4; 
+		uint32_t instruction = (state.memory[pc+3]<<24) + (state.memory[pc+2]<<16) + (state.memory[pc+1]<<8) 
+			+ state.memory[pc]; 
+		state.registers.state_access.PC = state.registers.state_access.PC + 4;
+		return instruction;
 }
-
