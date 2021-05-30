6ae925b53a84d5a3feec7957a8292d353a59c6bb
wz1620 <wz1620@imperial.ac.uk>
2021-05-30 16:48:22 +0100

    add branch instruction

---
+++ src/branch.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "definitions.h"
+
+void branch(struct State *state_ptr, struct Instruction *instruction_ptr){
+			uint32_t offset = instruction_ptr->offset;
+			//Left shift 2 bits (equivalent to *2^2)
+			offset *= 4;
+			//Sign extension by 6 bits (equivalent to + 11111100 00000000 00000000 00000000) 
+			if (offset >> 25){
+					offset += 4227858432;
+			state_ptr->registers.PC += offset;
+}
