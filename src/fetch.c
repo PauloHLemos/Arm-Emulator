742c631aeedefa751caf9f50c77e3781e243fb3a
wz1620 <wz1620@imperial.ac.uk>
2021-05-31 18:11:27 +0100

    Weizhong: correct fetch

--- src/fetch.c
+++ src/fetch.c
@@ -1,10 +1,10 @@
  #include <stdint.h>
  #include "definitions.h"
 
-uint32_t fetch(State *state) {
-		uint32_t pc = state.registers.state_access.PC/4; 
-		uint32_t instruction = (state.memory[pc+3]<<24) + (state.memory[pc+2]<<16) + (state.memory[pc+1]<<8) 
-			+ state.memory[pc]; 
-		state.registers.state_access.PC = state.registers.state_access.PC + 4;
+uint32_t fetch(struct State *state_ptr) {
+		uint32_t pc = state_ptr->registers.struct_access.PC/4; 
+		uint32_t instruction = (state_ptr->memory[pc+3]<<24) + (state_ptr->memory[pc+2]<<16) 
+			+ (state_ptr->memory[pc+1]<<8) + state_ptr->memory[pc]; 
+		state_ptr->registers.struct_access.PC += 4;
 		return instruction;
 }
