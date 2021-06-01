11f8d31aa8ec3c2d7a4f0edce399da59e83ec57d
Otto White <ow20@ic.ac.uk>
2021-06-01 03:56:34 +0100

    Otto: add offset shift to branch, still need to ensure offset is a signed effect to PC

--- src/branch.c
+++ src/branch.c
@@ -4,12 +4,13 @@
 #include "instructions.h"
 
 void branch(struct State *state_ptr, struct Instruction *instruction_ptr){
-			uint32_t offset = instruction_ptr->offset;
-			//Left shift 2 bits (equivalent to *2^2)
-			offset *= 4;
-			//Sign extension by 6 bits (equivalent to + 11111100 00000000 00000000 00000000) 
-			if (offset >> 25){
-					offset += 0b11111100000000000000000000000000;
-			state_ptr->registers.struct_access.PC += offset;
-			}
+	uint32_t offset = instruction_ptr->offset;
+	offset <<= 2;
+	if ((offset & (1 << 25)) != 0) {
+		offset &= 0b11111100000000000000000000000000;
+	}
+	//Sign extension by 6 bits (equivalent to + 11111100 00000000 00000000 00000000) 
+
+	//TODO: make sure PC is getting the signed effect of the offset
+	state_ptr->registers.struct_access.PC += (offset - 8);
 }
