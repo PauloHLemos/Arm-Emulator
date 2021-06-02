3e693fbf6e438e72d1d48e7d6b0df618f483801e
Otto White <ow20@ic.ac.uk>
2021-06-02 19:23:14 +0100

    Otto: improve style in execute

--- src/Emulate/fetch.c
+++ src/Emulate/fetch.c
@@ -4,10 +4,10 @@
 uint32_t fetch(struct State *state_ptr) {
 	uint32_t pc = state_ptr->registers.struct_access.PC; 
 	uint32_t instruction = 
-		(state_ptr->memory[pc+3]<<24) + 
-		(state_ptr->memory[pc+2]<<16) + 
-		(state_ptr->memory[pc+1]<<8) + 
-		state_ptr->memory[pc]; 
+			(state_ptr->memory[pc+3]<<24) + 
+			(state_ptr->memory[pc+2]<<16) + 
+			(state_ptr->memory[pc+1]<<8) + 
+			state_ptr->memory[pc]; 
 
 	state_ptr->registers.struct_access.PC += 4; 
 	return instruction; 
