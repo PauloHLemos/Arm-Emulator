f797bdf08ac6e14dfe019271f10fa5c79ac75225
Otto White <ow20@ic.ac.uk>
2021-06-02 13:51:40 +0100

    Otto: fix bitwise operation in branch

--- src/Emulate/branch.c
+++ src/Emulate/branch.c
@@ -7,7 +7,7 @@ void branch(struct State *state_ptr, struct Instruction *instruction_ptr){
 	uint32_t offset = instruction_ptr->offset;
 	offset <<= 2;
 	if ((offset & (1 << 25)) != 0) {
-		offset &= 0b11111100000000000000000000000000;
+		offset |= 0b11111100000000000000000000000000;
 	}
 	//Sign extension by 6 bits (equivalent to + 11111100 00000000 00000000 00000000) 
 
