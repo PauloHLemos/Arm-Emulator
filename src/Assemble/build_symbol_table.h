65765c3c5a1e8ff448c5a9c40fa1892c9c08697e
Robert Stok <rs1720@texel10.doc.ic.ac.uk>
2021-06-02 15:02:37 +0100

    Rob: move all header files for Assemble into the new Assemble directory.

---
+++ src/Assemble/build_symbol_table.h
@@ -0,0 +1,4 @@
+#include "symbol_table.h"
+
+struct *ST_Node build_symbol_table([file stream reader]);
+
---
+++ src/Assemble/translate_single_data_transfer.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+#include "symbol_table.h"
+
+uint32_t translate_single_data_transfer(char *instruction, struct ST_Node *st_head_ptr);
