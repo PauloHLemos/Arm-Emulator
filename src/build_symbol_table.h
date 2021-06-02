1dc89b9f3dbe891b2dd5fbf60af471a37e2e4210
Robert Stok <rs1720@texel10.doc.ic.ac.uk>
2021-06-02 15:14:59 +0100

    Rob: Move assemble header files into new Assemble folder

--- src/build_symbol_table.h
+++
@@ -1,4 +0,0 @@
-#include "symbol_table.h"
-
-struct *ST_Node build_symbol_table([file stream reader]);
-
--- src/translate.h
+++
@@ -1,3 +0,0 @@
-#include "symbol_table.h"
-
-void translate([file stream reader], struct ST_Node *st_head_ptr);
