da75841b8c9dfd77bd79ed09f88f7301c202c398
Otto White <ow20@ic.ac.uk>
2021-06-02 19:11:10 +0100

    Otto: improve style and remove magic numbers in decode

--- src/Emulate/decode.c
+++ src/Emulate/decode.c
@@ -8,11 +8,11 @@ struct Instruction decode(uint32_t instruction_binary) {
 	memset(&instruction, 0, sizeof(struct Instruction));
 	instruction.cond = instruction_binary >> 28;
 	
-	bool bit27 = (instruction_binary & 134217728) != 0;
-	bool bit26 = (instruction_binary & 67108864) != 0;
-	bool bit25 = (instruction_binary & 33554432) != 0;
-	bool bit7 = (instruction_binary & 128) != 0;
-	bool bit4 = (instruction_binary & 16) != 0;
+	bool bit27 = (instruction_binary & 1 << 27) != 0;
+	bool bit26 = (instruction_binary & 1 << 26) != 0;
+	bool bit25 = (instruction_binary & 1 << 25) != 0;
+	bool bit7  = (instruction_binary & 1 << 7) != 0;
+	bool bit4  = (instruction_binary & 1 << 4) != 0;
 
 	if (bit27) {
 		instruction.type = BRANCH;
@@ -25,49 +25,49 @@ struct Instruction decode(uint32_t instruction_binary) {
 	}
 
 	if (instruction.type == DATA_PROCESSING) {
-		uint32_t immediate_bitmask = 33554432;
-		uint32_t opcode_bitmask = 31457280;
-		uint32_t set_codes_bitmask = 1048576;
-		uint32_t rn_bitmask = 983040;
-		uint32_t rd_bitmask = 61440;
-		uint32_t operand2_bitmask = 4095;
-		instruction.immediate_operand = (instruction_binary & immediate_bitmask) != 0;
-		instruction.opcode = (instruction_binary & opcode_bitmask) >> 21;
+		uint32_t immediate_bitmask	= 1 << 25;
+		uint32_t opcode_bitmask	   	= 0xf << 21;
+		uint32_t set_codes_bitmask 	= 1 << 20;
+		uint32_t rn_bitmask	   	= 0xf << 16;
+		uint32_t rd_bitmask 	   	= 0xf << 12;
+		uint32_t operand2_bitmask  	= 0xfff;
+		instruction.immediate_operand	= (instruction_binary & immediate_bitmask) != 0;
+		instruction.opcode		= (instruction_binary & opcode_bitmask) >> 21;
 		instruction.set_condition_codes = (instruction_binary & set_codes_bitmask) != 0;
-		instruction.rn = (instruction_binary & rn_bitmask) >> 16;
-		instruction.rd = (instruction_binary & rd_bitmask) >> 12;
-		instruction.operand2 = instruction_binary & operand2_bitmask;
+		instruction.rn			= (instruction_binary & rn_bitmask) >> 16;
+		instruction.rd			= (instruction_binary & rd_bitmask) >> 12;
+		instruction.operand2		= instruction_binary & operand2_bitmask;
 	} else if (instruction.type == MULTIPLY) {
-		uint32_t set_codes_bitmask = 1048576;
-		uint32_t accumulate_bitmask = 2097152;
-		uint32_t rd_bitmask = 983040;
-		uint32_t rn_bitmask = 61440;
-		uint32_t rs_bitmask = 3840;
-		uint32_t rm_bitmask = 15;
+		uint32_t accumulate_bitmask 	= 1 << 21;
+		uint32_t set_codes_bitmask	= 1 << 20;
+		uint32_t rd_bitmask	    	= 0xf << 16;
+		uint32_t rn_bitmask	    	= 0xf << 12;
+		uint32_t rs_bitmask	    	= 0xf << 8;
+		uint32_t rm_bitmask	    	= 0xf;
 		instruction.set_condition_codes = (instruction_binary & set_codes_bitmask) != 0;
-		instruction.accumulate = (instruction_binary & accumulate_bitmask) != 0;
-		instruction.rd = (instruction_binary & rd_bitmask) >> 16;
-		instruction.rn = (instruction_binary & rn_bitmask) >> 12;
-		instruction.rs = (instruction_binary & rs_bitmask) >> 8;
-		instruction.rm = instruction_binary & rm_bitmask;
+		instruction.accumulate		= (instruction_binary & accumulate_bitmask) != 0;
+		instruction.rd			= (instruction_binary & rd_bitmask) >> 16;
+		instruction.rn			= (instruction_binary & rn_bitmask) >> 12;
+		instruction.rs			= (instruction_binary & rs_bitmask) >> 8;
+		instruction.rm			= instruction_binary & rm_bitmask;
 	} else if (instruction.type == SINGLE_DATA_TRANSFER) {
-		uint32_t immediate_bitmask = 33554432;
-		uint32_t indexing_bitmask = 16777216;
-		uint32_t up_bitmask = 8388608;
-		uint32_t load_store_bitmask = 1048576;
-		uint32_t rn_bitmask = 983040;
-		uint32_t rd_bitmask = 61440;
-		uint32_t offset_bitmask = 4095;
-		instruction.immediate_offset = (instruction_binary & immediate_bitmask) != 0;
+		uint32_t immediate_bitmask    = 1 << 25;
+		uint32_t indexing_bitmask     = 1 << 24;
+		uint32_t up_bitmask	      = 1 << 23;
+		uint32_t load_store_bitmask   = 1 << 20;
+		uint32_t rn_bitmask	      = 0xf << 16;
+		uint32_t rd_bitmask	      = 0xf << 12;
+		uint32_t offset_bitmask	      = 0xfff;
+		instruction.immediate_offset  = (instruction_binary & immediate_bitmask) != 0;
 		instruction.pre_post_indexing = (instruction_binary & indexing_bitmask) != 0;
-		instruction.up = (instruction_binary & up_bitmask) != 0;
-		instruction.load_store = (instruction_binary & load_store_bitmask) != 0;
-		instruction.rn = (instruction_binary & rn_bitmask) >> 16;
-		instruction.rd = (instruction_binary & rd_bitmask) >> 12;
-		instruction.offset = instruction_binary & offset_bitmask;
+		instruction.up		      = (instruction_binary & up_bitmask) != 0;
+		instruction.load_store	      = (instruction_binary & load_store_bitmask) != 0;
+		instruction.rn		      = (instruction_binary & rn_bitmask) >> 16;
+		instruction.rd 		      = (instruction_binary & rd_bitmask) >> 12;
+		instruction.offset	      = instruction_binary & offset_bitmask;
 	} else {
-		uint32_t offset_bitmask = 16777215;
-		instruction.offset = instruction_binary & offset_bitmask;
+		uint32_t offset_bitmask = 0xffffff;
+		instruction.offset	= instruction_binary & offset_bitmask;
 	}
 
 	return instruction;
