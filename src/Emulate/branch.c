a5b49b0b950d96571ce0db6635cad0290abe1ba9
Otto White <ow20@ic.ac.uk>
2021-06-02 18:02:24 +0100

    Otto: branch simplifications and clean up

--- src/Emulate/branch.c
+++ src/Emulate/branch.c
@@ -4,13 +4,7 @@
 #include "instructions.h"
 
 void branch(struct State *state_ptr, struct Instruction *instruction_ptr){
-	uint32_t offset = instruction_ptr->offset;
-	offset <<= 2;
-	if ((offset & (1 << 25)) != 0) {
-		offset |= 0b11111100000000000000000000000000;
-	}
-	//Sign extension by 6 bits (equivalent to + 11111100 00000000 00000000 00000000) 
-
-	//TODO: make sure PC is getting the signed effect of the offset
+	uint32_t offset = instruction_ptr->offset << 2;
+	if ((offset & (1 << 25)) != 0) offset |= 0b11111100000000000000000000000000;
 	state_ptr->registers.struct_access.PC += offset;
 }
