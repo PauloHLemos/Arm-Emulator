07441e149eb4a401d09dca287bc16c7827f46701
Otto White <ow20@ic.ac.uk>
2021-06-04 12:26:21 +0100

    Otto: fix test.c

--- src/Assemble/test.c
+++ src/Assemble/test.c
@@ -5,25 +5,25 @@
 
 bool assert_true(bool condition, char *error_message) {
 	if (!condition) {
-		printf(char);
-		return False;
+		printf("%s\n", error_message);
+		return false;
 	}
-	return True;
+	return true;
 }
 
 bool assert_int_equals(uint32_t value, uint32_t expected, char *error_message) {
 	if (value != expected) {
-		printf(char);
-		return False;
+		printf("%s\n", error_message);
+		return false;
 	}
-	return True;
+	return true;
 }
 
 
 // example test function
 bool test_somefile(void) {
 	// run your function in here
-	assert_true(1 == 1, "FAIL: somefile.somefunction is not working."
+	assert_true(1 == 1, "FAIL: somefile.somefunction is not working.");
 }
 
 
