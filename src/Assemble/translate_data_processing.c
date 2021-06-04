df2cee4b0c64023118bb4e953e92aaf316b7c676
Otto White <ow20@ic.ac.uk>
2021-06-04 16:25:37 +0100

    Otto: add test.h file to allow usage of test suite

--- src/Assemble/translate_data_processing.c
+++ src/Assemble/translate_data_processing.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "definitions.h"
+#include "test.h"
 // #include "instructions.h"
 
 
 uint32_t translate_data_processing(char *instruction/*, struct ST_Node *st_head_ptr*/) {
 	// Examples
+	printf("Hello\n");
 
 	return 0;
 }
 
 int main(void) {
-	printf("Hello");
+	
+	translate_data_processing("addeq r1, r5");
 	return 0;
 }
 
