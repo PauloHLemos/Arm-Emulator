2f1cc657a10e390655d9b9d70f4f204b8a782323
Otto White <ow20@ic.ac.uk>
2021-06-04 17:13:18 +0100

    Otto: create opcode extraction function for data processing instructions

--- src/Assemble/translate_data_processing.c
+++ src/Assemble/translate_data_processing.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "definitions.h"
 #include "test.h"
+#include "string.h"
 // #include "instructions.h"
 
+enum Opcode extract_opcode(char *opcode_string) {
+	if	(strcmp(opcode_string, "and") == 0) { return AND; } 
+	else if (strcmp(opcode_string, "eor") == 0) { return EXCLUSIVE_OR; }
+	else if (strcmp(opcode_string, "sub") == 0) { return SUBTRACT; }
+	else if (strcmp(opcode_string, "rsb") == 0) { return REVERSE_SUBTRACT; }
+	else if (strcmp(opcode_string, "add") == 0) { return ADD; }
+	else if (strcmp(opcode_string, "orr") == 0) { return OR; }
+	else if (strcmp(opcode_string, "mov") == 0) { return MOVE; }
+	else if (strcmp(opcode_string, "tst") == 0) { return TEST_BITS; }
+	else if (strcmp(opcode_string, "teq") == 0) { return TEST_EQUALS; }
+	else if (strcmp(opcode_string, "cmp") == 0) { return COMPARE; }
+	else { fprintf(stderr, "Opcode \"%s\" does not exist. \n", opcode_string); exit(1); return -1; }
+}
 
 uint32_t translate_data_processing(char *instruction/*, struct ST_Node *st_head_ptr*/) {
-	// Examples
-	printf("Hello\n");
+	// Make andeq a special case
+	
+	// struct Instruction instruction_struct;
+	
+	// this won't catch andeq, it will just chop off the eq
+	char opcode_string[3];
+	memcpy(opcode_string, instruction, 3);
+	enum Opcode opcode = extract_opcode(opcode_string);
 
 	return 0;
 }
 
 int main(void) {
-	
-	translate_data_processing("addeq r1, r5");
+	translate_data_processing("poo r1, r5");
 	return 0;
 }
 
