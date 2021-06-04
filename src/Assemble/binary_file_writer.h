51109d8567f819adc7f563d9adc9102125041a3e
Otto White <ow20@ic.ac.uk>
2021-06-04 12:34:11 +0100

    Otto: add ifndefs to all header files

--- src/Assemble/binary_file_writer.h
+++ src/Assemble/binary_file_writer.h
@@ -1,3 +1,8 @@
+#ifndef BINARY_FILE_WRITER_H
+#define BINARY_FILE_WRITER_H
+
 [stream obj] open_file_stream();
 bool close_file_stream([stream obj]);
 bool write_word([stream obj] file_stream, uint32_t word);
+
+#endif
--- src/Assemble/convert_instructions.h
+++ src/Assemble/convert_instructions.h
@@ -1,4 +1,9 @@
+#ifndef CONVERT_INSTRUCTIONS_H
+#define CONVERT_INSTRUCTIONS_H
+
 #include <stdint.h>
 #include "symbol_table.h"
 
 uint32_t convert_instruction(char *instruction, struct ST_Node *st_head_ptr, uint32_t current_address);
+
+#endif
