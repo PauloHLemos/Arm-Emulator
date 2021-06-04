91b6599f78479256a03cb98c6fda53812708c411
Otto White <ow20@ic.ac.uk>
2021-06-04 18:58:10 +0100

    Otto: make a 4 argument separation function

--- src/Assemble/translate_data_processing.c
+++ src/Assemble/translate_data_processing.c
@@ -20,15 +20,48 @@ enum Opcode extract_opcode(char *opcode_string) {
 	else { fprintf(stderr, "Opcode \"%s\" does not exist. \n", opcode_string); exit(1); return -1; }
 }
 
+void print_at_separators(char *string, char *arg1, char *arg2, char *arg3, char *arg4) {
+	for ( ; *string != ' '; string += 1) {
+		strncat(arg1, string, 1);
+	}
+	string += 1;
+
+	for ( ; *string != ','; string += 1) {
+		strncat(arg2, string, 1);
+	}
+	string += (*(string + 1) == ' ') ? 2 : 1;
+
+	for ( ; *string != ','; string += 1) {
+		strncat(arg3, string, 1);
+	}
+	string += (*(string + 1) == ' ') ? 2 : 1;
+
+	for ( ; *string != '\0'; string += 1) {
+		strncat(arg4, string, 1);
+	}
+}
+
 uint32_t translate_data_processing(char *instruction/*, struct ST_Node *st_head_ptr*/) {
 	// Make andeq a special case
 	
 	// struct Instruction instruction_struct;
 	
 	// this won't catch andeq, it will just chop off the eq
-	char opcode_string[3];
-	memcpy(opcode_string, instruction, 3);
-	enum Opcode opcode = extract_opcode(opcode_string);
+	char opcode_string[30] = "sub r1, r2    , r3, lsl r4";
+
+	char arg1[100] = "";
+	char arg2[100] = "";
+	char arg3[100] = "";
+	char arg4[100] = "";
+
+	print_at_separators(opcode_string, &arg1, &arg2, &arg3, &arg4);
+
+	printf("%s\n", arg1);
+	printf("%s\n", arg2);
+	printf("%s\n", arg3);
+	printf("%s\n", arg4);
+
+
 
 	return 0;
 }
