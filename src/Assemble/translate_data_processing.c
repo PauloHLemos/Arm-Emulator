1e4f56e75ad9732411c27f604771b28fb572d4b4
Otto White <ow20@ic.ac.uk>
2021-06-09 04:11:29 +0100

    Otto: add the shifted register processing, all tests passing

--- src/Assemble/translate_data_processing.c
+++ src/Assemble/translate_data_processing.c
@@ -63,31 +63,40 @@ uint32_t process_operand2_shifted_register(char *operand2_string) {
 
 	for ( ; *operand2_string != ',' && *operand2_string != '\0'; operand2_string += 1) 
 			strncat(shift_register_string, operand2_string, 1);
+	
+	// register no shift case
+	operand2 += atoi(shift_register_string + 1);
+	if (*operand2_string == '\0') return operand2;
 
-	if (*operand2_string == '\0') {
-		// register case
-		printf("Shift register: %s\n", shift_register_string);
-		return atoi(shift_register_string + 1);
-	} else {
-		// optional shift case
+	char shift_type_string[10] = "";
+	char shift_amount_string[20] = "";
+	operand2_string += (*(operand2_string + 1) == ' ') ? 2 : 1;
+	for ( ; *operand2_string != ' '; operand2_string += 1) 
+			strncat(shift_type_string, operand2_string, 1);
+	operand2_string += 1;
+	for ( ; *operand2_string != '\0'; operand2_string += 1) 
+			strncat(shift_amount_string, operand2_string, 1);
+	
 
-		char shift_type_string[10] = "";
-		char shift_amount_string[20] = "";
-		operand2_string += (*(operand2_string + 1) == ' ') ? 2 : 1;
-		for ( ; *operand2_string != ' '; operand2_string += 1) 
-				strncat(shift_type_string, operand2_string, 1);
-		operand2_string += 1;
-		for ( ; *operand2_string != '\0'; operand2_string += 1) 
-				strncat(shift_amount_string, operand2_string, 1);
+	// cases on next letter of shift_amount_string
+	if	(strcmp(shift_type_string, "lsl") == 0)	{  }
+	else if (strcmp(shift_type_string, "lsr") == 0) { operand2 += (1 << 5); }
+	else if (strcmp(shift_type_string, "asr") == 0) { operand2 += (2 << 5); }
+	else if (strcmp(shift_type_string, "ror") == 0) { operand2 += (3 << 5); }
 
-		printf("Shift register: %s\n", shift_register_string);
-		printf("Shift type: %s\n", shift_type_string);
-		printf("Shift amount: %s\n", shift_amount_string);
+	if (*shift_amount_string == 'r') {
+		operand2 += (1 << 4); // signifies is a shifted reg
+		uint32_t shift_register = atoi(shift_amount_string + 1);
+		assert(shift_register < (1 << 4));
+		operand2 += (shift_register << 8);
+
+	} else if (*shift_amount_string == '#') {
+		uint32_t immediate_shift = atoi(shift_amount_string + 1);
+		assert(immediate_shift < (1 << 5));
+		operand2 += (immediate_shift << 7);
 	}
 
-
-
-	return 0;
+	return operand2;
 }
 
 void process_operand2(char *operand2_string, uint32_t *operand2_ptr, bool *immediate_operand_ptr) {
