a5e2db74d2a28588ef194d79d14244d60f1e32b5
Otto White <ow20@ic.ac.uk>
2021-06-09 17:53:54 +0100

    Otto: fully create test suite for encode

--- src/Assemble/test_encode.c
+++ src/Assemble/test_encode.c
@@ -1,7 +1,40 @@
 #include "test_encode.h"
 #include "encode.h"
 #include "definitions.h"
-#define UNUSED 0
+
+void print_binary(uint32_t number) {
+	char buffer[100] = "";
+        for (uint32_t i = 0, mask = 1 << 31; i < 32; i++, number <<= 1) {
+		if (i % 4 == 0 && i > 0) {
+			strncat(buffer, " ", 1);
+		}
+                strncat(buffer, ((number & mask) != 0) ? "1" : "0", 1);
+        }
+	printf("%s\n", buffer);
+}
+
+void test_binary_equals(char *test_name, char *instruction, uint32_t expected, 
+		uint32_t got, uint32_t *tests_run_ptr, uint32_t *tests_passed_ptr) {
+	(*tests_run_ptr)++;	
+
+	printf("--------------------------------------------------------------\n"
+	       "Test name:\t%s\n"
+	       "Instruction:\t%s\n" 
+	       "Status:\t\t%s\n", 
+	       test_name, instruction, (expected == got) ? "Passed" : "Failed"); 
+	
+	if (expected == got)  {
+		(*tests_passed_ptr)++;
+	} else {
+		printf("\nExpected:\t");
+		print_binary(expected);
+		printf("Got:\t\t");	    
+		print_binary(got);
+	}
+
+	printf("--------------------------------------------------------------\n");
+
+}
 
 void test_encode(void) {
 	// DATA PROCESSING	       MULTIPLY			   SINGLE DATA TRANSFER	       BRANCH
@@ -22,11 +55,19 @@ void test_encode(void) {
 	// uint32_t operand2;                                                                                     
 	//                                                         uint32_t offset;            uint32_t offset;  
 	
+	uint32_t tests_run = 0;
+	uint32_t tests_passed = 0;
+
 	struct Instruction instruction;
 	instruction.type = DATA_PROCESSING;
 	instruction.cond = ALWAYS;
 	instruction.opcode = ADD;
 
-	uint32_t result = encode(instruction);
-	printf("Test result: %d\n", result);
+	test_binary_equals("Test test", "andtests test r0", 0b00000000000000000000000000000000, 
+			encode(instruction), &tests_run, &tests_passed);
+
+	test_binary_equals("Test test 2", "thing test r0", 0b00110000010000000000000000110000, 
+			encode(instruction), &tests_run, &tests_passed);
+
+	printf("\n%d of %d tests passed correctly. \n", tests_passed, tests_run);
 }
