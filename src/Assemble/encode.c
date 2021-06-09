1bf3949e68e814747465cd1a67e417f7f88bd187
Otto White <ow20@ic.ac.uk>
2021-06-09 22:02:09 +0100

    Otto: add default bit, register and flag setting in encode

--- src/Assemble/encode.c
+++ src/Assemble/encode.c
@@ -1,20 +1,86 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "encode.h"
 #include "definitions.h"
 #include "test_encode.h"
 
+void set_bit(uint32_t *number, char bit_no, bool bit_value) {
+	if (bit_value) {
+		*number |= (1 << bit_no);
+	} else {
+		*number &= ~(1 << bit_no);
+	}
+}
+
 uint32_t encode_data_processing(struct Instruction instruction) {
-	return 0;
+	uint32_t instruction_binary = 0;
+	
+	// Flag setting
+	set_bit(&instruction_binary, 20, instruction.set_condition_codes); // S bit
+	set_bit(&instruction_binary, 25, instruction.immediate_operand);   // I bit
+
+	// Register setting
+	assert(instruction.rd < (1 << 4));
+	assert(instruction.rn < (1 << 4));
+	instruction_binary |= (instruction.rd << 12);
+	instruction_binary |= (instruction.rn << 16);
+
+	
+	return instruction_binary;
 }
+
 uint32_t encode_multiply(struct Instruction instruction) {
-	return 0;
+	uint32_t instruction_binary = 0;
+	
+	// Setting default bits
+	set_bit(&instruction_binary, 4, 1);
+	set_bit(&instruction_binary, 7, 1);
+	
+	// Flag setting	
+	set_bit(&instruction_binary, 20, instruction.set_condition_codes); // S bit
+	set_bit(&instruction_binary, 21, instruction.accumulate);	   // A bit
+	
+	// Register setting
+	assert(instruction.rn < (1 << 4));
+	assert(instruction.rd < (1 << 4));
+	assert(instruction.rs < (1 << 4));
+	assert(instruction.rm < (1 << 4));
+	instruction_binary |= (instruction.rm);
+	instruction_binary |= (instruction.rs << 8);
+	instruction_binary |= (instruction.rn << 12);
+	instruction_binary |= (instruction.rd << 16);
+
+	return instruction_binary;
 }
+
 uint32_t encode_single_data_transfer(struct Instruction instruction) {
-	return 0;
+	uint32_t instruction_binary = 0;
+	// Setting default bits
+	set_bit(&instruction_binary, 26, 1);
+
+	// Flag setting
+	set_bit(&instruction_binary, 20, instruction.load_store);	 // L bit
+	set_bit(&instruction_binary, 23, instruction.up);		 // U bit
+	set_bit(&instruction_binary, 24, instruction.pre_post_indexing); // P bit
+	set_bit(&instruction_binary, 25, instruction.immediate_offset);  // I bit
+	
+	// Register setting
+	assert(instruction.rd < (1 << 4));
+	assert(instruction.rn < (1 << 4));
+	instruction_binary |= (instruction.rd << 12);
+	instruction_binary |= (instruction.rn << 16);
+
+	return instruction_binary;
 }
+
 uint32_t encode_branch(struct Instruction instruction) {
-	return 0;
+	uint32_t instruction_binary = 0;
+	// Setting default bits
+	set_bit(&instruction_binary, 25, 1);
+	set_bit(&instruction_binary, 27, 1);
+
+	return instruction_binary;
 }
 
 uint32_t encode(struct Instruction instruction) {
