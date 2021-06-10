556ef3cb3f69c4009c83033855adbd8c075c23af
Otto White <ow20@ic.ac.uk>
2021-06-10 01:10:12 +0100

    Otto: add reading stdin arguments in assemble

--- src/Assemble/assemble.c
+++ src/Assemble/assemble.c
@@ -1,5 +1,22 @@
 #include <stdlib.h>
+#include <stdio.h>
+// #include "build_symbol_table.h"
+// #include "translate.h"
+// #include "symbol_table.h"
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+	if (argc < 3) {
+		printf("Not enough arguments. \n");
+		return EXIT_FAILURE;
+	} else if (argc > 3) {
+		printf("Too many arguments. \n");
+		return EXIT_FAILURE;
+	}
+
+	char *input_filename = *(argv + 1);
+	char *output_filename = *(argv + 2);
+	
+	printf("Input filename: %s\nOutput filename: %s\n", input_filename, output_filename);
+
+	return EXIT_SUCCESS;
 }
