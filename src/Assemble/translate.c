0bf584fd6ac2b33914ed153f6f6a11fe08965089
Robert Stok <rs1720@texel10.doc.ic.ac.uk>
2021-06-11 22:29:23 +0100

    Rob: add output_filename argument to translate

--- src/Assemble/translate.c
+++ src/Assemble/translate.c
@@ -6,13 +6,12 @@
 
 bool is_label(char *instruction);
 
-void translate(FILE *stream, struct ST_Node *st_head_ptr) {
+void translate(FILE *stream, struct ST_Node *st_head_ptr, char *output_filename) {
 	int max_line_length = 512;
 	char buffer[max_line_length];
 	uint32_t current_address = 0;
 	uint32_t translated_instruction;
-	char *file_name = "name";
-	FILE *output_stream = open_file(file_name);
+	FILE *output_stream = open_file(output_filename);
 
 	while(fgets(buffer, max_line_length, stream)) {
 		if (!is_label(buffer)) {
--- src/Assemble/translate.h
+++ src/Assemble/translate.h
@@ -3,6 +3,6 @@
 
 #include "symbol_table.h"
 
-void translate(FILE *stream, struct ST_Node *st_head_ptr);
+void translate(FILE *stream, struct ST_Node *st_head_ptr, char *output_filename);
 
 #endif
