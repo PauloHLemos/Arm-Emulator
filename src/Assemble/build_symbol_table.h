8f110efd111807009c8ab3a6208bfe902a4231e1
Otto White <ow20@ic.ac.uk>
2021-06-11 17:01:40 +0100

    Otto: fix library compilation in Makefile for symbol table

--- src/Assemble/build_symbol_table.h
+++ src/Assemble/build_symbol_table.h
@@ -1,7 +1,7 @@
 #ifndef BUILD_SYMBOL_TABLE_H
 #define BUILD_SYMBOL_TABLE_H
 
-#include "symbol_table.h"
+#include "lib/symbol_table.h"
 #include <stdio.h>
 
 struct ST_Node *build_symbol_table(FILE *stream);
