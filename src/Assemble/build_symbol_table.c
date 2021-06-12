49d65378db9b2df7b0a59853407de615d9aa5140
Paulo Lemos <pl2420@ic.ac.uk>
2021-06-12 22:58:10 +0100

    Paulo: fix address counter and add address pointer in build_symbol_table

--- src/Assemble/build_symbol_table.c
+++ src/Assemble/build_symbol_table.c
@@ -4,7 +4,7 @@
 #include "build_symbol_table.h"
 #include "symbol_table.h"
 
-struct ST_Node *build_symbol_table(FILE *stream) {
+struct ST_Node *build_symbol_table(FILE *stream, uint32_t *address_ptr) {
 	struct ST_Node* head = initialize();
 	// head->address
 	char *label;
@@ -16,9 +16,11 @@ struct ST_Node *build_symbol_table(FILE *stream) {
 		label = strtok(buffer, ":");
 		if (label != NULL) {
 			add_node(head, label, address);	
+		} else {
+			address += 4;
 		}		
-		address += 4;
 	}
+	*address_ptr = address;
 	return head;
 }
 
