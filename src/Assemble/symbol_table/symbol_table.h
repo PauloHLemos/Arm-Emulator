9c7c26600775eeb87f6a09da06c12ec5c8c45681
Paulo Lemos <pl2420@ic.ac.uk>
2021-06-12 22:48:58 +0100

    Paulo: change return type of search_table to bool

--- src/Assemble/symbol_table/symbol_table.h
+++ src/Assemble/symbol_table/symbol_table.h
@@ -8,7 +8,7 @@ struct ST_Node {
 	struct ST_Node *next_ptr;
 };
 
-uint32_t search_table(struct ST_Node *node, char *label);
+bool search_table(struct ST_Node *node, char *label, uint32_t *address_ptr);
 void add_node(struct ST_Node *node, char *label, uint32_t address);
 struct ST_Node *initialize();
 void dealocate(struct ST_Node *node);
