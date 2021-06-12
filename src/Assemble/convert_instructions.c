335e9926ec6ccf591acd0270f03fad4f806fad2f
Robert Stok <rs1720@texel10.doc.ic.ac.uk>
2021-06-12 01:42:59 +0100

    Rob: fix compile time errors in convert_instructions

--- src/Assemble/convert_instructions.c
+++ src/Assemble/convert_instructions.c
@@ -7,9 +7,9 @@
 
 bool is_multiply(char *opcode);
 bool is_single_data_transfer(char *opcode);
-bool is branch(char *opcode);
+bool is_branch(char *opcode);
 
-uint32_t translate_instruction(char *instruction, ST_Node *st_head_ptr, uint32_t current_address) {
+uint32_t convert_instructions(char *instruction, struct ST_Node *st_head_ptr, uint32_t current_address) {
 	struct Instruction instruction_struct;
 	char opcode[3];
 	extract_opcode(instruction, opcode);
@@ -18,7 +18,7 @@ uint32_t translate_instruction(char *instruction, ST_Node *st_head_ptr, uint32_t
 	} else if (is_single_data_transfer(opcode)) {
 		instruction_struct = translate_single_data_transfer(instruction);
 	} else if (is_branch(opcode)) {
-		instruction_struct = translate_branch(instruction, st_head_ptr);
+		instruction_struct = translate_branch(instruction, st_head_ptr, current_address);
 	} else {
 		instruction_struct = translate_data_processing(instruction);
 	}
