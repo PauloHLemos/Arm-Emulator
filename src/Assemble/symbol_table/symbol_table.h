e2dc9af74dd2940041c89b2a2a3c0b7c844c5ee7
Robert Stok <rs1720@texel10.doc.ic.ac.uk>
2021-06-12 18:21:45 +0100

    Rob: import stdlib in translate_multiply and remove commented code in symbol_table

--- src/Assemble/symbol_table/symbol_table.h
+++ src/Assemble/symbol_table/symbol_table.h
@@ -11,14 +11,9 @@ struct ST_Node {
 	struct ST_Node *next_ptr;
 };
 
-<<<<<<< HEAD:src/Assemble/symbol_table.h
-// uint32_t search_table(struct ST_Node *node, char *label);
-// bool add_node(struct ST_Node *node, char *label, uint32_t address);
-
-#endif
-=======
 uint32_t search_table(struct ST_Node *node, char *label);
 void add_node(struct ST_Node *node, char *label, uint32_t address);
 struct ST_Node *initialize();
 void dealocate(struct ST_Node *node);
->>>>>>> implement_symbol_table_lib:src/Assemble/symbol_table/symbol_table.h
+
+#endif
--- src/Assemble/translate_multiply.c
+++ src/Assemble/translate_multiply.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 #include "split_instructions.h"
 #include "instructions.h"
 #include "definitions.h"
