a8c4dfff2b345fcea32e7d8f43be8a6e428fb991
Otto White <ow20@ic.ac.uk>
2021-06-13 06:30:47 +0100

    Otto: add missing binary file writer to makefile and add binary file writer .h to source code

--- src/Assemble/binary_file_writer.c
+++ src/Assemble/binary_file_writer.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "binary_file_writer.h"
 
 void binary_file_writer(FILE *file_ptr, uint32_t *word_ptr){
         fwrite(word_ptr, 1, 4, file_ptr);
