a51392a2fe2b0cae72ceca13b17980a02a0b6bb9
Otto White <ow20@ic.ac.uk>
2021-06-13 03:04:39 +0100

    Otto: fix makefile and include sdio.h in binary file writer

--- src/Assemble/binary_file_writer.h
+++ src/Assemble/binary_file_writer.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_FILE_WRITER_H
 #define BINARY_FILE_WRITER_H
 
+#include <stdio.h>
+
 FILE *open_file(char *binary_fname);
 void close_file(FILE *file_ptr);
 void binary_file_writer(FILE *file_ptr, uint32_t *word_ptr);
