674588feffb5c043f1cbc81437e0008bf8c46116
Otto White <ow20@ic.ac.uk>
2021-06-13 05:53:19 +0100

    Otto: Decapitalise queue type name

--- src/Assemble/queue.c
+++ src/Assemble/queue.c
@@ -1,31 +1,31 @@
 #include <assert.h>
 #include "queue.h"
 
-struct QUEUE_Node get_queue_node(struct QUEUE_Node *node, uint32_t *binary_ptr) {
+struct Queue_Node get_queue_node(struct Queue_Node *node, uint32_t *binary_ptr) {
 	assert(node != NULL);
 	*binary_ptr = node->binary;
-	struct QUEUE_Node next_ptr = node->next_ptr;
+	struct Queue_Node next_ptr = node->next_ptr;
 	free(node);
 	return next_ptr;
 }
 
-void add_queue_node(struct QUEUE_Node *node, uint32_t binary) {
+void add_queue_node(struct Queue_Node *node, uint32_t binary) {
 	while (node->next_ptr != NULL) {
 		node = node->next_ptr;
 	}
-	struct QUEUE_NODE *new_node = initialize_queue();
+	struct Queue_NODE *new_node = initialize_queue();
 	new_node->binary = binary;
 	node->next_ptr = new_node;
 
 }
 
-static struct QUEUE_Node* allocate_queue_node() {
-	struct QUEUE_Node *node = malloc(sizeof(node));
+static struct Queue_Node* allocate_queue_node() {
+	struct Queue_Node *node = malloc(sizeof(node));
 	return node;
 }
 
-struct QUEUE_Node *initialize_queue() {
-	struct QUEUE_Node *node = allocate_queue_node();
+struct Queue_Node *initialize_queue() {
+	struct Queue_Node *node = allocate_queue_node();
 	node->next_ptr == NULL;
 	return node;
 }
--- src/Assemble/queue.h
+++ src/Assemble/queue.h
@@ -1,18 +1,18 @@
-#ifndef QUEUE_H 
-#define QUEUE_H
+#ifndef Queue_H 
+#define Queue_H
 
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-struct QUEUE_Node {
+struct Queue_Node {
 	uint32_t binary;
-	struct QUEUE_Node *next_ptr;
+	struct Queue_Node *next_ptr;
 };
 
-struct QUEUE_Node get_queue_node(struct QUEUE_Node *node, uint32_t *binary_ptr);
-void add_queue_node(struct QUEUE_Node *node, uint32_t binary);
-struct QUEUE_Node *initialize_queue();
+struct Queue_Node get_queue_node(struct Queue_Node *node, uint32_t *binary_ptr);
+void add_queue_node(struct Queue_Node *node, uint32_t binary);
+struct Queue_Node *initialize_queue();
 
 #endif
 
