17cef5197a1ddc689f15de8b1d13eac1f4e7886c
Otto White <ow20@ic.ac.uk>
2021-06-13 05:39:41 +0100

    Otto: rename queue functions to resolve naming conflict

--- src/Assemble/queue.c
+++ src/Assemble/queue.c
@@ -1,7 +1,7 @@
 #include <assert.h>
 #include "queue.h"
 
-struct QUEUE_Node get_node(struct QUEUE_Node *node, uint32_t *binary_ptr) {
+struct QUEUE_Node get_queue_node(struct QUEUE_Node *node, uint32_t *binary_ptr) {
 	assert(node != NULL);
 	*binary_ptr = node->binary;
 	struct QUEUE_Node next_ptr = node->next_ptr;
@@ -9,23 +9,23 @@ struct QUEUE_Node get_node(struct QUEUE_Node *node, uint32_t *binary_ptr) {
 	return next_ptr;
 }
 
-void add_node(struct QUEUE_Node *node, uint32_t binary) {
+void add_queue_node(struct QUEUE_Node *node, uint32_t binary) {
 	while (node->next_ptr != NULL) {
 		node = node->next_ptr;
 	}
-	struct QUEUE_NODE *new_node = initialize();
+	struct QUEUE_NODE *new_node = initialize_queue();
 	new_node->binary = binary;
 	node->next_ptr = new_node;
 
 }
 
-static struct QUEUE_Node* allocate_node() {
+static struct QUEUE_Node* allocate_queue_node() {
 	struct QUEUE_Node *node = malloc(sizeof(node));
 	return node;
 }
 
-struct QUEUE_Node *initialize() {
-	struct QUEUE_Node *node = allocate_node();
+struct QUEUE_Node *initialize_queue() {
+	struct QUEUE_Node *node = allocate_queue_node();
 	node->next_ptr == NULL;
 	return node;
 }
--- src/Assemble/queue.h
+++ src/Assemble/queue.h
@@ -10,9 +10,9 @@ struct QUEUE_Node {
 	struct QUEUE_Node *next_ptr;
 };
 
-struct QUEUE_Node get_node(struct QUEUE_Node *node, uint32_t *binary_ptr);
-void add_node(struct QUEUE_Node *node, uint32_t binary);
-struct QUEUE_Node *initialize();
+struct QUEUE_Node get_queue_node(struct QUEUE_Node *node, uint32_t *binary_ptr);
+void add_queue_node(struct QUEUE_Node *node, uint32_t binary);
+struct QUEUE_Node *initialize_queue();
 
 #endif
 
