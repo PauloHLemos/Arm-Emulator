5bfbc6197e13f5f36f2720c6cea300a1b20cd457
Otto White <ow20@ic.ac.uk>
2021-06-13 06:02:47 +0100

    Otto: pass queue pointer rather than actual queue into single data transfer

--- src/Assemble/convert_instructions.c
+++ src/Assemble/convert_instructions.c
@@ -14,20 +14,20 @@ bool is_branch(char *opcode);
 
 uint32_t convert_instructions(char *instruction, struct ST_Node *st_head_ptr, uint32_t current_address, uint32_t end_address) {
 	struct Instruction instruction_struct;
-	struct Queue_Node queue_node = initialize_queue();
+	struct Queue_Node *queue_node_ptr = initialize_queue();
 	char opcode[3];
 	extract_opcode(instruction, opcode);
 	if (is_multiply(opcode)) {
 		instruction_struct = translate_multiply(instruction);
 	} else if (is_single_data_transfer(opcode)) {
-		instruction_struct = translate_single_data_transfer(instruction, queue_node, current_address, end_address);
+		instruction_struct = translate_single_data_transfer(instruction, queue_node_ptr, current_address, end_address);
 	} else if (is_branch(opcode)) {
 		instruction_struct = translate_branch(instruction, st_head_ptr, current_address);
 	} else {
 		instruction_struct = translate_data_processing(instruction);
 	}
 
-	// deallocate_queue(queue_node);
+	// deallocate_queue(queue_node_ptr);
 	return encode(instruction_struct);
 }
 
