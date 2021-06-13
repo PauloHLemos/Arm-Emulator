1a158043b5ad0828a4dff6e43670d9a102e79e4a
Robert Stok <rs1720@texel10.doc.ic.ac.uk>
2021-06-13 03:04:05 +0100

    Rob: reverse order of bytes in translate before writing, increment address by 4 instead of 1

--- src/Assemble/translate.c
+++ src/Assemble/translate.c
@@ -6,6 +6,8 @@
 
 bool is_label(char *instruction);
 
+uint32_t reverse_bytes(uint32_t bytes);
+
 void translate(FILE *stream, struct ST_Node *st_head_ptr, char *output_filename) {
 	int max_line_length = 512;
 	char buffer[max_line_length];
@@ -16,11 +18,19 @@ void translate(FILE *stream, struct ST_Node *st_head_ptr, char *output_filename)
 	while(fgets(buffer, max_line_length, stream)) {
 		if (!is_label(buffer)) {
 			translated_instruction = convert_instruction(buffer, st_head_ptr, current_address);
-			binary_file_writer(output_stream, translated_instruction);
-			current_address += 1;
+			binary_file_writer(output_stream, reverse_bytes(translated_instruction));
+			current_address += 4;
 		}		
 	}
 	close_file(output_stream);
+}
+
+uint32_t reverse_bytes(uint32_t bytes) {
+	int32_t temp = input << 24;
+	temp = temp | ((input & 0b1111111100000000) << 8);
+	temp = temp | ((input >> 8) & 0b1111111100000000);
+	temp = temp | ((input >> 24) & 0b11111111);
+	return temp;
 }
 
 bool is_label(char *instruction) {
