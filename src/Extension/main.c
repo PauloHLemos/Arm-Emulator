746bdbb97b3bd114c8c00198f855b1901496ac98
Otto White <ow20@ic.ac.uk>
2021-06-16 04:57:47 +0100

    Otto: get convulutions working

--- src/Extension/main.c
+++ src/Extension/main.c
@@ -6,9 +6,14 @@
 #include "image_manipulation.h"
 
 int main(void) {
+       int kernel[5][5] = { {  1,  0,  1,  0,  1 },
+			    {  0,  1,  0,  1,  0 },
+       			    {  1,  0,  1,  0,  1 },
+       			    {  0,  1,  0,  1,  0 },
+       			    {  1,  0,  1,  0,  1 } };
 	Frame *frame_ptr = load_image("samples/aeroplane.png", 3);
-	Frame greyscale_frame = rgb_to_greyscale(frame_ptr);
-	store_image(&greyscale_frame, "test_out.png");
+	Frame convolved_image = convolve_image(frame_ptr, kernel);
+	store_image(&convolved_image, "test_out.png");
 	//deallocate_frame(&greyscale_frame);
 	deallocate_frame(frame_ptr);
 	return 0;
