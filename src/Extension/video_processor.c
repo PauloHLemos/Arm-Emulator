8c7198734b603160aaf546d7f3edbb4ddefb7b54
Paulo Lemos <pl2420@ic.ac.uk>
2021-06-16 22:38:02 +0100

    Paulo: implement buffer for video loading

--- src/Extension/video_processor.c
+++ src/Extension/video_processor.c
@@ -1,20 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdint.h>
 #include "definitions.h"
+// #include "video_processor.h"
 
-// Video resolution
-#define W 1280
-#define H 720
- 
-// Allocate a buffer to store one frame
-unsigned char frame[H][W][3] = {0};
- 
-void process_video(const char* input_path, const char* output_path /*, array of function pointers*/) {
+static int get_width(const char* input_path) {
+	char get_dim_command[200];
+	sprintf(get_dim_command, "ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 %s", input_path);
+
+	FILE *dim_file;
+	char dimension[200];
+
+	dim_file = popen(get_dim_command, "r");
+	assert(dim_file != NULL);
+	fgets(dimension, sizeof(dimension), dim_file);
+	pclose(dim_file);
+	return atoi(dimension);
+}
+
+static int get_height(const char* input_path) {
+	char get_dim_command[200];
+	sprintf(get_dim_command, "ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 %s", input_path);
+
+	FILE *dim_file;
+	char dimension[200];
+
+	dim_file = popen(get_dim_command, "r");
+	assert(dim_file != NULL);
+	fgets(dimension, sizeof(dimension), dim_file);
+	pclose(dim_file);
+	return atoi(dimension);
+}
+
+void process_video(const char* input_path, const char* output_path, int buffer_size /*, array of function pointers*/) {
 	int x, y, count;
+	int width = get_width(input_path);
+	int height = get_height(input_path);
 
-	// Open an input pipe from ffmpeg and an output pipe to a second instance of ffmpeg
-	
 	char make_output_file_command[200];
 	char in_command[200];
 	char out_command[200];
@@ -22,26 +45,38 @@ void process_video(const char* input_path, const char* output_path /*, array of 
 	sprintf(make_output_file_command, "rm %s; touch %s; chmod +rwx %s\n", output_path, output_path, output_path);
 	sprintf(in_command, "ffmpeg -i %s -f image2pipe -vcodec rawvideo -pix_fmt rgb24 -", input_path);
 	sprintf(out_command, "ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x720 -r 25 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 %s", output_path);
+
 	printf("make output file command: %s\n", make_output_file_command);
 	printf("in command: %s\n", in_command);
 	printf("out command: %s\n", out_command);
+	
+	// Open an input pipe from ffmpeg and an output pipe to a second instance of ffmpeg
 	system(make_output_file_command);
 	FILE *pipein = popen(in_command, "r");
 	assert(pipein != NULL);
 	FILE *pipeout = popen(out_command, "w");
 	assert(pipeout != NULL);
 
-	for (count = fread(frame, 1, H*W*3, pipein); count  == H*W*3; count = fread(frame, 1, H*W*3, pipein)) {
+	uint8_t frame[height * width * 3];
+	// apparently just this declaration by itself breaks the code
+	// uint8_t frame_buffer[buffer_size][height * width * 3];
+	uint8_t *buffer[buffer_size];
+	for (int i = 0; i < buffer_size; i++) {
+		buffer[i] = (uint8_t *) malloc(height * width * 3 * sizeof(uint8_t));
+	}
 
-		for (y=0 ; y<H ; ++y) for (x=0 ; x<W ; ++x) {
-		    // Invert each colour component in every pixel
-		    frame[y][x][0] = 255 - frame[y][x][0]; // red
-		    frame[y][x][1] = 255 - frame[y][x][1]; // green
-		    frame[y][x][2] = 255 - frame[y][x][2]; // blue
+	int buffer_index = 0;
+	for (count = fread(frame, 1, height * width * 3, pipein);
+		count  == height * width * 3; count = fread(frame, 1, height * width * 3, pipein)) {
+
+		buffer[buffer_index] = frame;
+		
+		for (int i = 0 ; i < width * height * 3; ++i) {
+		    buffer[buffer_index][i] = 255 - buffer[buffer_index][i];
 		}
 		 
-		// Write this frame to the output pipe
-		fwrite(frame, 1, H*W*3, pipeout);
+		fwrite(buffer[buffer_index], 1, height * width * 3, pipeout);
+		buffer_index = (buffer_index + 1) % buffer_size;
 	}
 
 	// Flush and close input and output pipes
@@ -52,5 +87,5 @@ void process_video(const char* input_path, const char* output_path /*, array of 
 }
 
 int main(void) {
-	process_video("samples/teapot.mp4", "samples/test_output.mp4");	
+	process_video("samples/teapot.mp4", "samples/test_output.mp4", 3);	
 }
